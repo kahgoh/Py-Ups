9acf1853bdff31ba80a613ef19887a2790e65460
Kah Goh <villastar@yahoo.com.au>
2020-06-19 22:07:53 +0800

    Update test to write samples in binary mode
    This is to ensure that the tests create files with the same content,
    regardless of which line ending is used. Previously, the tests would
    sometimes fail because the expected hash and file size depended on which
    line endings was being used.

--- tests/state/test_state_calculation.py
+++ tests/state/test_state_calculation.py
@@ -1,3 +1,4 @@
+import base64
 import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -8,30 +9,30 @@ from pyups.state import model as state
 
 CONTENT = {
     "document": {
-        "content": 
-"""tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
-    Creates a temporary directory in the most secure
-    manner possible. There are no race conditions in
-    the directory’s creation. The directory is 
-    readable, writable, and searchable only by the
-    creating user ID.""",
-        "size": 284,
-        "hash": "eaf3f759ab59919ee454f1fc176cb8b17f828296"
+        "content": "dGVtcGZpbGUubWtkdGVtcChzdWZmaXg9Tm9uZSwgcHJlZml4PU5vbmUsIGRpcj1Ob25lKQogICAgQ3JlYXRlcyBhIHRlbXBvcmFyeSBkaXJlY3RvcnkgaW4gdGhlIG1vc3Qgc2VjdXJlCiAgICBtYW5uZXIgcG9zc2libGUuIFRoZXJlIGFyZSBubyByYWNlIGNvbmRpdGlvbnMgaW4KICAgIHRoZSBkaXJlY3RvcnnigJlzIGNyZWF0aW9uLiBUaGUgZGlyZWN0b3J5IGlzIAogICAgcmVhZGFibGUsIHdyaXRhYmxlLCBhbmQgc2VhcmNoYWJsZSBvbmx5IGJ5IHRoZQogICAgY3JlYXRpbmcgdXNlciBJRC4="
+            # tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+            #    Creates a temporary directory in the most secure
+            #    manner possible. There are no race conditions in
+            #    the directory’s creation. The directory is 
+            #    readable, writable, and searchable only by the
+            #    creating user ID.
+        , "size": 281
+        , "hash": "ba67180097e58a2b2037f66e60d7620cebb7fa4a"
     }, 
     "story": {
-        "content":
-"""Once upon a time there were three bears who lived in a
-house in the forest. Therewas a great big father bear, a 
-middle-sized mother bear and a tiny baby bear. One 
-morning, their breakfast porridge was too hot to eat, so
-they decided to go for a walk in the forest.""",
-        "size": 270,
-        "hash": "4a13688a37665c912d3f982e6304692f7bee66e1"
+        "content": "T25jZSB1cG9uIGEgdGltZSB0aGVyZSB3ZXJlIHRocmVlIGJlYXJzIHdobyBsaXZlZCBpbiBhCmhvdXNlIGluIHRoZSBmb3Jlc3QuIFRoZXJld2FzIGEgZ3JlYXQgYmlnIGZhdGhlciBiZWFyLCBhIAptaWRkbGUtc2l6ZWQgbW90aGVyIGJlYXIgYW5kIGEgdGlueSBiYWJ5IGJlYXIuIE9uZSAKbW9ybmluZywgdGhlaXIgYnJlYWtmYXN0IHBvcnJpZGdlIHdhcyB0b28gaG90IHRvIGVhdCwgc28KdGhleSBkZWNpZGVkIHRvIGdvIGZvciBhIHdhbGsgaW4gdGhlIGZvcmVzdC4="
+            # Once upon a time there were three bears who lived in a
+            # house in the forest. Therewas a great big father bear, a 
+            # middle-sized mother bear and a tiny baby bear. One 
+            # morning, their breakfast porridge was too hot to eat, so
+            # they decided to go for a walk in the forest.
+        , "size": 266
+        , "hash": "6274c99919fa355879cfc43838c718cf231356ed"
     }, 
     "names": {
-        "content": "Adam Eve Jack Jill Hansel Gretel",
-        "size": 32,
-        "hash": "31222fa4058e916488289a3c41e0a75a57fc6947"
+        "content": "QWRhbSBFdmUgSmFjayBKaWxsIEhhbnNlbCBHcmV0ZWw="
+        , "size": 32
+        , "hash": "31222fa4058e916488289a3c41e0a75a57fc6947"
     }
 }
 
@@ -49,8 +50,9 @@ def sample(request) -> dict:
     content["temporary_directory"] = temporary_directory
     content["path"] = path
 
-    with path.open(mode="w") as file:
-        file.write(content["content"])
+    with path.open(mode="wb") as file:
+        decodedBytes = base64.standard_b64decode(content["content"])
+        file.write(decodedBytes)
 
     logging.info(f"Sample data stored in: {path}")
 
